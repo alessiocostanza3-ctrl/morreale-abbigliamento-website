34ee88f35c7680f9f8b17f5e52b55aa9c165c235
alessiocostanza3-ctrl <alessiocostanza3@gmail.com>
2026-05-28 13:20:07 +0200

    Compilazione assets per layout modale centrato e reattivo

--- assets/index-DKa9QseO.js
+++ assets/index-DKa9QseO.js
@@ -1432,7 +1432,7 @@ Error generating stack: `+e.message+`
         }
 
         .modal-image-col {
-          height: 600px;
+          height: min(600px, 65vh);
           background-color: var(--bg-secondary);
         }
 
